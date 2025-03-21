541b2828333fa301c221c86c3bc7c7bb434d1c25
Tejas0041 <tejaspawar70238@gmail.com>
2025-03-21 06:50:32 +0530

    chnged dev->productoin mode

--- app.py
+++ app.py
@@ -108,4 +108,5 @@ def remove_category(name):
     return jsonify({"message": "Category and associated expenses deleted"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
